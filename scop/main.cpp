31a486448fc9ed1e70cb8159185c2c2f635c1468
gymnopediese <kalityoflife@gmail.com>
2026-06-01 13:47:44 +0200

    Initial Commit

---
+++ scop/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  scop
+//
+//  Created by joe hikari on 01.06.2026.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
